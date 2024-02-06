8b7b8b290c0183c8e38e343747d930e1d3c6da13
Arthur Gibert <arthurgibert@free.fr>
2024-02-06 14:00:14 +0100

    Ajout de la classe OrderDao

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 
 import fr.fms.dao.CategoryDao;
 import fr.fms.dao.FormationDao;
+import fr.fms.dao.UserDao;
 import fr.fms.entities.Category;
 import fr.fms.entities.Formation;
+import fr.fms.entities.User;
 
 public class TestApplication {
 	
 	public static void main(String[] args) {
-		CategoryDao dao = new CategoryDao();
-		for(Category formation : dao.readAll()) {
+		UserDao dao = new UserDao();
+		for(User formation : dao.readAll()) {
 			System.out.println(formation);
 		}
 	}
