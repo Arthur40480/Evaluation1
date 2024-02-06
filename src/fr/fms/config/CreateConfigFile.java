ff4e2eb3f40634d184121f02b87e80d1dafdf7e6
Arthur Gibert <arthurgibert@free.fr>
2024-02-06 12:51:04 +0100

    Ajout de la classe BddConnection

--- src/fr/fms/config/CreateConfigFile.java
+++ src/fr/fms/config/CreateConfigFile.java
@@ -1,4 +1,4 @@
-package fr.fms.entities;
+package fr.fms.config;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,12 +8,14 @@ import java.util.Properties;
 public class CreateConfigFile {
 	public static void main(String[] args) {
 		try (OutputStream ops = new FileOutputStream("files/config.properties")) {		
-			Properties properties = new Properties();	
+			Properties properties = new Properties();
+			
 			properties.setProperty("db.driver", "org.mariadb.jdbc.Driver");
 			properties.setProperty("db.url", "jdbc:mariadb://localhost:3306/Shop");
 			properties.setProperty("db.login", "root");
 			properties.setProperty("db.password", "ArthurGibertTosse40230");			
-			properties.store(ops , "No comment !");
+			properties.store(ops , "Fichier de Configuration");
+			
 			System.out.println("Fichier de configuration créer avec succès !");
 		}
 		 catch (IOException io) {
