731dce00e196f8329a92726a5918493044d7999e
Arthur Gibert <arthurgibert@free.fr>
2024-02-06 12:34:09 +0100

    Ajout de la classe CreateConfigFile

---
+++ src/fr/fms/entities/CreateConfigFile.java
@@ -0,0 +1,23 @@
+package fr.fms.entities;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class CreateConfigFile {
+	public static void main(String[] args) {
+		try (OutputStream ops = new FileOutputStream("files/config.properties")) {		
+			Properties properties = new Properties();	
+			properties.setProperty("db.driver", "org.mariadb.jdbc.Driver");
+			properties.setProperty("db.url", "jdbc:mariadb://localhost:3306/Shop");
+			properties.setProperty("db.login", "root");
+			properties.setProperty("db.password", "ArthurGibertTosse40230");			
+			properties.store(ops , "No comment !");
+			System.out.println("Fichier de configuration créer avec succès !");
+		}
+		 catch (IOException io) {
+	            io.printStackTrace();
+		}
+	}
+}
