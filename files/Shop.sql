2f10d7285a9ee4f8ce441cea090c10a42ef685c1
Arthur Gibert <arthurgibert@free.fr>
2024-02-06 13:36:55 +0100

    Ajout de la classe FormationDao

--- files/Shop.sql
+++ files/Shop.sql
@@ -10,13 +10,13 @@ Use Shop;
 -- - Construction de la table des utilisateurs                               ---
 -- -----------------------------------------------------------------------------
 CREATE TABLE T_Users (
-	idUser				int(4)		PRIMARY KEY AUTO_INCREMENT,
-	login				varchar(20)	NOT NULL UNIQUE,
-	password			varchar(20)	NOT NULL
+	IdUser				int(4)		PRIMARY KEY AUTO_INCREMENT,
+	Login				varchar(20)	NOT NULL UNIQUE,
+	Password			varchar(20)	NOT NULL
 ) ENGINE = InnoDB;
 
-INSERT INTO T_Users (idUser, login, password) VALUES ( 1, 'Ambre' ,	'1234' );
-INSERT INTO T_Users (idUser, login, password) VALUES ( 2, 'Arthur',	'azerty' );
+INSERT INTO T_Users (IdUser, Login, Password) VALUES ( 1, 'Ambre' ,	'1234' );
+INSERT INTO T_Users (IdUser, Login, Password) VALUES ( 2, 'Arthur',	'azerty' );
 
 SELECT * FROM T_Users;
 
@@ -26,14 +26,14 @@ SELECT * FROM T_Users;
 -- -----------------------------------------------------------------------------
 
 CREATE TABLE T_Customers (
-	idCustomer				int(4)		PRIMARY KEY AUTO_INCREMENT,
-	name					varchar(30)	NOT NULL,
-	firstName				varchar(30)	NOT NULL,
-	email 					varchar(30)	NOT NULL unique,
-	phone 					varchar(20)	,
-	address					varchar(50)	,
-	idUser					int(4)		NOT NULL,
-	FOREIGN KEY (idUser)	REFERENCES T_Users(idUser)
+	IdCustomer				int(4)		PRIMARY KEY AUTO_INCREMENT,
+	Name					varchar(30)	NOT NULL,
+	FirstName				varchar(30)	NOT NULL,
+	Email 					varchar(30)	NOT NULL unique,
+	Phone 					varchar(20)	,
+	Address					varchar(50)	,
+	IdUser					int(4)		NOT NULL,
+	FOREIGN KEY (IdUser)	REFERENCES T_Users(IdUser)
 ) ENGINE = InnoDB;
 
 
@@ -41,15 +41,15 @@ CREATE TABLE T_Customers (
 -- - Construction de la table des Categories d'article - --
 -- ---------------------------------------------------------------------
 CREATE TABLE T_Categories (
-	idCategory 			INT(4)		PRIMARY KEY AUTO_INCREMENT,
-	catName				VARCHAR(30)	NOT NULL,
-	description			VARCHAR(100)	NOT NULL
+	IdCategory 			INT(4)		PRIMARY KEY AUTO_INCREMENT,
+	CatName				VARCHAR(30)	NOT NULL,
+	Description			VARCHAR(100)	NOT NULL
 ) ENGINE = InnoDB;
 
-INSERT INTO T_Categories (idCategory, catName, description) VALUES ( 1, 'Dev Web'	, 'Formation axée sur le développement web');
-INSERT INTO T_Categories (idCategory, catName, description) VALUES ( 2, 'CMS'	, 'Formation axée sur le système de gestion de contenu');
-INSERT INTO T_Categories (idCategory, catName, description) VALUES ( 3, 'IA'	, 'Formation axée sur l''intelligence artificielle');
-INSERT INTO T_Categories (idCategory, catName, description) VALUES ( 4, 'Cyber Sécurité', 'Formation axée sur la cyber sécurité');
+INSERT INTO T_Categories (IdCategory, CatName, Description) VALUES ( 1, 'Dev Web'	, 'Formation axée sur le développement web');
+INSERT INTO T_Categories (IdCategory, CatName, Description) VALUES ( 2, 'CMS'	, 'Formation axée sur le système de gestion de contenu');
+INSERT INTO T_Categories (IdCategory, CatName, Description) VALUES ( 3, 'IA'	, 'Formation axée sur l''intelligence artificielle');
+INSERT INTO T_Categories (IdCategory, CatName, Description) VALUES ( 4, 'Cyber Sécurité', 'Formation axée sur la cyber sécurité');
 
 SELECT * FROM T_Categories;
 
@@ -58,25 +58,25 @@ SELECT * FROM T_Categories;
 -- - Construction de la table des formations en vente - --
 -- ---------------------------------------------------------------------
 CREATE TABLE T_Formations (
-	idFormation			INT(4)			PRIMARY KEY AUTO_INCREMENT,
-	name				VARCHAR(50)		NOT NULL,
-	description			VARCHAR(100)	NOT NULL,
-	duration 			INT(4)			NOT NULL,
-	remote				BOOLEAN			NOT NULL,
-	unitaryPrice		FLOAT(8)		NOT NULL,
-	idCategory			INT(4)			NOT NULL,
-	FOREIGN KEY (idCategory)			REFERENCES T_Categories(idCategory)
+	IdFormation			INT(4)			PRIMARY KEY AUTO_INCREMENT,
+	Name				VARCHAR(50)		NOT NULL,
+	Description			VARCHAR(100)	NOT NULL,
+	Duration 			INT(4)			NOT NULL,
+	Remote				BOOLEAN			NOT NULL,
+	UnitaryPrice		FLOAT(8)		NOT NULL,
+	IdCategory			INT(4)			NOT NULL,
+	FOREIGN KEY (IdCategory)			REFERENCES T_Categories(IdCategory)
 ) ENGINE = InnoDB;
 
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Développement Web Full Stack', 'Formation complète sur le développement web, couvrant à la fois frontend et backend', 30, true, 1000, 1);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Marketing Digital Avancé', 'Approfondissement des stratégies de marketing en ligne, SEO SEM et médias sociaux', 20, false, 850, 2);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Data Science Fondamental', 'Introduction aux concepts fondamentaux de la science des données', 12, false, 2500, 3);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Gestion de Projet Agile ', 'Formation pratique sur les méthodologies de gestion de projet Agile', 42, false, 4900, 1);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Certificat AWS Cloud', 'Préparation de l''examen de certification AWS Cloud', 18, true, 1500, 1);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'LeaderShip et gestion d''équipe', 'Développement des compétences en leadership et en gestion d''équipe', 8, false, 900, 1);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Développement d''Application Mobiles IOS', 'Apprentissage du développement d''applications IOS ', 24, true, 6000, 1);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Introduction à l''IA', 'Aperçu des concepts clés de l''intelligence artificielle', 22, false, 3200, 3);
-INSERT INTO T_Formations ( name, description, duration, remote, unitaryPrice, idCategory ) VALUES ( 'Sécurité des Réseaux Informatique', 'Formation approfondie sur la sécurité des réseaux informatiques', 40, true, 8300, 4);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Développement Web Full Stack', 'Formation complète sur le développement web, couvrant à la fois frontend et backend', 30, true, 1000, 1);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Marketing Digital Avancé', 'Approfondissement des stratégies de marketing en ligne, SEO SEM et médias sociaux', 20, false, 850, 2);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Data Science Fondamental', 'Introduction aux concepts fondamentaux de la science des données', 12, false, 2500, 3);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Gestion de Projet Agile ', 'Formation pratique sur les méthodologies de gestion de projet Agile', 42, false, 4900, 1);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Certificat AWS Cloud', 'Préparation de l''examen de certification AWS Cloud', 18, true, 1500, 1);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'LeaderShip et gestion d''équipe', 'Développement des compétences en leadership et en gestion d''équipe', 8, false, 900, 1);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Développement d''Application Mobiles IOS', 'Apprentissage du développement d''applications IOS ', 24, true, 6000, 1);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Introduction à l''IA', 'Aperçu des concepts clés de l''intelligence artificielle', 22, false, 3200, 3);
+INSERT INTO T_Formations ( Name, Description, Duration, Remote, UnitaryPrice, IdCategory ) VALUES ( 'Sécurité des Réseaux Informatique', 'Formation approfondie sur la sécurité des réseaux informatiques', 40, true, 8300, 4);
 
 SELECT * FROM T_Formations;
 
@@ -85,11 +85,26 @@ SELECT * FROM T_Formations;
 -- - Construction de la table des commandes - --
 -- ---------------------------------------------------------------------
 CREATE TABLE T_Orders (
-	idOrder				int(4)		PRIMARY KEY AUTO_INCREMENT,
-	amount				float(4)	NOT NULL DEFAULT 0,
-	dateOrder 			DATE		NOT NULL DEFAULT NOW(),
-	idCustomer          INT(4)   	NOT NULL,
+	IdOrder				int(4)		PRIMARY KEY AUTO_INCREMENT,
+	Amount				float(4)	NOT NULL DEFAULT 0,
+	DateOrder 			DATE		NOT NULL DEFAULT NOW(),
+	IdCustomer          INT(4)   	NOT NULL,
 	FOREIGN KEY(idCustomer) REFERENCES T_Customers(idCustomer)
 ) ENGINE = InnoDB;
 
+-- ---------------------------------------------------------------------
+-- - Construction de la table des items de commande - --
+-- ---------------------------------------------------------------------
+CREATE TABLE T_Order_Items (
+	IdOrderItem			int(4)	PRIMARY KEY AUTO_INCREMENT,
+	
+	IdArticle           INT(4)   NOT NULL,
+	FOREIGN KEY(IdArticle) REFERENCES T_Articles(IdArticle),
+	
+	Quantity			FLOAT(4) NOT NULL DEFAULT 1,
+	UnitaryPrice		FLOAT(4)	NOT NULL DEFAULT 0,
+	
+	IdOrder             INT(4)   NOT NULL,
+	FOREIGN KEY(IdOrder) REFERENCES T_Orders(IdOrder)
+) ENGINE = InnoDB;
 
---
+++ src/fr/fms/dao/FormationDao.java
@@ -0,0 +1,147 @@
+package fr.fms.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import fr.fms.entities.Article;
+import fr.fms.entities.Formation;
+
+public class FormationDao implements Dao<Formation>{
+
+	/**
+	 * Méthode qui crée un article en base sans prendre en compte l'id (généré automatiquement)
+	 * @param Formation à ajouter dans la table des formations
+	 */
+	@Override
+	public boolean create(Formation obj) {
+		String str = "INSERT INTO T_Formations (Name, Description, Duration, Remote, UnitaryPrice, IdCategory) VALUES (?,?,?,?,?,?);";	
+		try (PreparedStatement ps = connection.prepareStatement(str)){
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getDescription());
+			ps.setInt(3, obj.getDuration());
+			ps.setBoolean(4, obj.isRemote());
+			ps.setDouble(5, obj.getUnitaryPrice());
+			ps.setInt(6, obj.getCategory());
+			if( ps.executeUpdate() == 1)	return true;
+		} catch (SQLException e) {
+			logger.severe("pb sql sur la création d'une formation " + e.getMessage());
+		} 	
+		return false;
+	}
+	
+	/**
+	 * Méthode qui renvoi toutes les infos d'une formation à partir de son id s'il existe dans la table T_Formations
+	 * @param id de la formation 
+	 * @return formation si trouvé, null sinon
+	 */
+	@Override
+	public Formation read(int id) {
+		try (Statement statement = connection.createStatement()){
+			String str = "SELECT * FROM T_Formations where IdFormation=" + id + ";";									
+			ResultSet rs = statement.executeQuery(str);
+			if(rs.next()) return new Formation(rs.getInt(1), rs.getString(2), rs.getString(3) , rs.getInt(4) , rs.getBoolean(5), rs.getDouble(6), rs.getInt(7));
+		} catch (SQLException e) {
+			logger.severe("pb sql sur la lecture d'une formation " + e.getMessage());
+		} 	
+		return null;
+	}
+	
+	/**
+	 * Méthode qui met à jour une formation si elle existe (à partir de son id) dans la table T_Formations
+	 * @param formation concerné
+	 * @return vrai si trouvé, faux sinon
+	 */
+	@Override
+	public boolean update(Formation obj) {
+		String str = "UPDATE T_Formations set Name=?, Description=?, Duration=?, Remote=?, UnitaryPrice=?, IdCategory=? where IdFormation=?;";
+		try (PreparedStatement ps = connection.prepareStatement(str)){				
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getDescription());
+			ps.setInt(3, obj.getDuration());
+			ps.setBoolean(4, obj.isRemote());
+			ps.setDouble(5, obj.getUnitaryPrice());
+			ps.setInt(6, obj.getCategory());
+			ps.setInt(7, obj.getId());
+			if( ps.executeUpdate() == 1)	return true;
+		} catch (SQLException e) {
+			logger.severe("pb sql sur la mise à jour d'une formation " + e.getMessage());
+		} 	
+		return false;
+	}
+	
+	/**
+	 * Méthode qui supprime une formation à partir de son id (s'il existe) dans la table T_Formations
+	 * @param formation concerné
+	 * @return vrai si suppression ok faux sinon
+	 */
+	@Override
+	public boolean delete(Formation obj) {
+		try (Statement statement = connection.createStatement()){
+			String str = "DELETE FROM T_Formations where IdFormation=" + obj.getId() + ";";									
+			statement.executeUpdate(str);		
+			return true;
+		} catch (SQLException e) {
+			logger.severe("pb sql sur la suppression d'une formation " + e.getMessage());
+		} 	
+		return false;
+	}
+	
+	/**
+	 * Méthode qui renvoi toutes les formations de la table T_Formations
+	 * @return liste de formations
+	 */
+	@Override
+	public ArrayList<Formation> readAll() {
+		ArrayList<Formation> formations = new ArrayList<Formation>();
+		String strSql = "SELECT * FROM T_Formations";		
+		try(Statement statement = connection.createStatement()){
+			try(ResultSet resultSet = statement.executeQuery(strSql)){ 			
+				while(resultSet.next()) {
+					int rsId = resultSet.getInt(1);	
+					String rsName = resultSet.getString(2);
+					String rsDescription = resultSet.getString(3);
+					int rsDuration = resultSet.getInt(4);
+					boolean rsRemote = resultSet.getBoolean(5);	
+					double rsUnitaryPrice = resultSet.getDouble(6);
+					int rsIdCategory = resultSet.getInt(7);
+					formations.add((new Formation(rsId, rsName, rsDescription, rsDuration, rsRemote, rsUnitaryPrice, rsIdCategory)));						
+				}	
+			}
+		} catch (SQLException e) {
+			logger.severe("pb sql sur renvoi de toutes les formations " + e.getMessage());
+		}	
+		return formations;
+	}
+	
+	/**
+	 * Méthode qui renvoi toute les formations d'une catégorie
+	 * @param id de la catégorie
+	 * @return liste de formation
+	 */
+	public ArrayList<Formation> readAllByCat(int id) {
+		ArrayList<Formation> formations = new ArrayList<Formation>();
+		String strSql = "SELECT * FROM T_Formations where idCategory=" + id;		
+		try(Statement statement = connection.createStatement()){
+			try(ResultSet resultSet = statement.executeQuery(strSql)){ 			
+				while(resultSet.next()) {
+					int rsId = resultSet.getInt(1);	
+					String rsName = resultSet.getString(2);
+					String rsDescription = resultSet.getString(3);
+					int rsDuration = resultSet.getInt(4);
+					boolean rsRemote = resultSet.getBoolean(5);	
+					double rsUnitaryPrice = resultSet.getDouble(6);
+					int rsIdCategory = resultSet.getInt(7);
+					formations.add((new Formation(rsId, rsName, rsDescription, rsDuration, rsRemote, rsUnitaryPrice, rsIdCategory)));						
+				}	
+			}
+		} catch (SQLException e) {
+			logger.severe("pb sql sur renvoi de toutes les formations d'une catégorie " + e.getMessage());
+		}			
+		return formations;
+	}
+
+}
+
