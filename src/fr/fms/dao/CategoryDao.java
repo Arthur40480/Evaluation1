47ebcf58e6735e1ee64209627610afd926166031
Arthur Gibert <arthurgibert@free.fr>
2024-02-07 11:39:30 +0100

    Ajout des méthode create, update et delete

--- src/fr/fms/dao/CategoryDao.java
+++ src/fr/fms/dao/CategoryDao.java
@@ -1,5 +1,6 @@
 package fr.fms.dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,12 +10,28 @@ import fr.fms.entities.Category;
 
 public class CategoryDao implements Dao<Category> {
 	
+	/**
+	 * Méthode qui crée une catégorie en base sans prendre en compte l'id (généré automatiquement)
+	 * @param categorie à ajouter dans la table des catégories
+	 */
 	@Override
 	public boolean create(Category obj) {
-		// TODO Auto-generated method stub
+		String str = "INSERT INTO T_Categories (CatName, Description) VALUES (?,?);";	
+		try (PreparedStatement ps = connection.prepareStatement(str)){
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getDescription());
+			if( ps.executeUpdate() == 1)	return true;
+		} catch (SQLException e) {
+			logger.severe("pb sql sur la création d'une catégorie " + e.getMessage());
+		} 	
 		return false;
 	}
-
+	
+	/**
+	 * Méthode qui renvoi toutes les infos d'une catégorie à partir de son id si elle existe dans la table T_Categories
+	 * @param id de la categorie 
+	 * @return categorie si trouvé, null sinon
+	 */
 	@Override
 	public Category read(int id) {
 		try (Statement statement = connection.createStatement()){
@@ -26,19 +43,47 @@ public class CategoryDao implements Dao<Category> {
 		} 	
 		return null;
 	}
-
+	
+	/**
+	 * Méthode qui met à jour une catégorie si elle existe (à partir de son id) dans la table T_Categories
+	 * @param Categorie concerné
+	 * @return vrai si trouvé, faux sinon
+	 */
 	@Override
 	public boolean update(Category obj) {
-		// TODO Auto-generated method stub
+		String str = "UPDATE T_Categories set CatName=?, Description=? where IdCategory=?;";
+		try (PreparedStatement ps = connection.prepareStatement(str)){				
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getDescription());
+			ps.setInt(3, obj.getId());
+			if( ps.executeUpdate() == 1)	return true;
+		} catch (SQLException e) {
+			logger.severe("pb sql sur la mise à jour d'une catégorie " + e.getMessage());
+		} 	
 		return false;
 	}
-
+	
+	/**
+	 * Méthode qui supprime une catégorie à partir de son id (s'il existe) dans la table T_Categories
+	 * @param categorie concerné
+	 * @return vrai si suppression ok faux sinon
+	 */
 	@Override
 	public boolean delete(Category obj) {
-		// TODO Auto-generated method stub
+		try (Statement statement = connection.createStatement()){
+			String str = "DELETE FROM T_Categories where IdCategory=" + obj.getId() + ";";									
+			statement.executeUpdate(str);		
+			return true;
+		} catch (SQLException e) {
+			logger.severe("pb sql sur la suppression d'une catégorie " + e.getMessage());
+		} 	
 		return false;
 	}
-
+	
+	/**
+	 * Méthode qui renvoi toutes les catégories de la table T_Categories
+	 * @return liste de catégories
+	 */
 	@Override
 	public ArrayList<Category> readAll() {
 		ArrayList<Category> categories = new ArrayList<Category>();
